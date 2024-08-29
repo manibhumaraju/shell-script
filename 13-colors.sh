97823ee32ddfac61d4d9bc3dd28d3ca5afe27a49
manibhumaraju <manishvarmab@gmail.com>
2024-08-29 19:33:35 +0530

    functions

--- 13-colors.sh
+++ 13-colors.sh
@@ -35,12 +35,12 @@ else
     echo "Git is already installed, nothing to do.."
 fi
 
-dnf list installed mysqlsssss
+dnf list installed mysql
 
 if [ $? -ne 0 ]
 then
     echo "MySQL is not installed...going to install"
-    dnf install mysqlssss -y
+    dnf install mysql -y
     VALIDATE $? "Installing MySQL"
 else
     echo "MySQL is already installed...nothing to do"
