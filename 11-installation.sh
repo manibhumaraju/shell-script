26e39bbb9e0960b659b10c11f9e76233cf95a982
manibhumaraju <manishvarmab@gmail.com>
2024-08-29 17:49:05 +0530

    functions

--- 11-installation.sh
+++ 11-installation.sh
@@ -26,3 +26,21 @@ then
 else
     echo "Git is already installed, nothing to do..."
 fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed, going to install it..."
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "MySQL installation is not success...check it"
+        exit 1
+    else
+        echo "MySQL installation is success"
+    fi
+else
+    echo "MySQL is already installed, nothing to do..."
+fi       
+
