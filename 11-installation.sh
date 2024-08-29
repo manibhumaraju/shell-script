f01f7783b9d8d235c40426e9198fbe9397b86116
manibhumaraju <manishvarmab@gmail.com>
2024-08-29 17:37:43 +0530

    functions

--- 11-installation.sh
+++ 11-installation.sh
@@ -10,12 +10,12 @@ then
 
 fi
 
-dnf list installed gittt
+dnf list installed git
 
 if [ $? -ne 0 ]
 then
     echo "Git is not installed, going to install it..."
-    dnf install gittt -y
+    dnf install git -y
     if [ $? -ne 0 ]
     then
         echo "Git installation is not success...check it"
