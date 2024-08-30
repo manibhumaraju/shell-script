edcb23f183210b786697f134885a7e49fa89eb05
manibhumaraju <manishvarmab@gmail.com>
2024-08-30 13:09:50 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -3,7 +3,7 @@
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%S)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME/$TIMESTAMP.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
