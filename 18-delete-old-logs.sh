c5b7f809a54617c19962c81cf8a3d9482001dbe9
manibhumaraju <manishvarmab@gmail.com>
2024-08-31 09:36:40 +0530

    shell

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -17,3 +17,8 @@ fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files: $FILES"
+
+while IFS= read -r line #IFS,internal field seperator, empty it will ignore while space. -r is for not to ignore special charectors like /
+do
+  echo "Deleting line: $line"
+done <<< $FILES
