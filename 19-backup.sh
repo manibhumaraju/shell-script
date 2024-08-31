049769a5dd4e763e97e9f9ee73f875352bfb8891
manibhumaraju <manishvarmab@gmail.com>
2024-08-31 16:12:48 +0530

    shell

--- 19-backup.sh
+++ 19-backup.sh
@@ -3,6 +3,7 @@
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} #if $3 is empty, default is 14 days
+TIMESTAMP=$(date +%Y-%m-%d-%H-%S)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -32,9 +33,16 @@ FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 
 echo "Files: $FILES"
 
-if [ ! -z $FILES ]
+if [ ! -z $FILES ] # true if FILES is empty, ! makes it expression false
 then 
     echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
+
+    #check if zip file is successfully created or not
+    if [ -f $ZIP_FILE ]
+    then 
+        echo "Successfully zipped files older than $DAYS"
 else
     echo "No files older than $DAYS"
 fi
